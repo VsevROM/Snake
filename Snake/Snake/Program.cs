1101d7d628246547cd5c4e2738db815733b96efd
VsevROM <VSEVrom@yandex.ru>
2017-02-19 14:19:29 +0200

    Урок 1 окно

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");//выводим сообщение в окне
+            Console.ReadLine();//программа не закроется сама пока пользователь не нажмет Enter
+        }
+    }
+}
