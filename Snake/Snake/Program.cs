d34961af5b01ec00cb5940c424e70141e99e6dc1
VsevROM <VSEVrom@yandex.ru>
2017-02-21 22:50:15 +0200

    Урок5. О памяти. Обекты в куче, примитивы в стеке.Чтопоследним запихнули то первым и вытолкнули

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,15 +10,69 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            point p1 = new point(1, 3, '*');// new point()это не только объект, но и вызов ф-ии КОНСТРУКТОР
-            // класс можт работать не только с данными, но и с методами, фун-ми
-            p1.Draw();//вызываем метод из класса point для нашей точки
+            int x = 1;
+            Func1(x);
+            Console.WriteLine("Call Func1. x = " + x);
 
-            point p2 = new point(4, 5, '$'); 
-            p2.Draw();
+            x = 1;
+            Func2(x);
+            Console.WriteLine("Call Func2. x = " + x);
 
-            Console.ReadLine();//программа не закроется сама пока пользователь не нажмет Enter
+            x = 1;
+            Func3(x);
+            Console.WriteLine("Call Func3. x = " + x);
+
+            Point p1 = new Point(1, 3, '*');
+            Move(p1, 10, 10);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
+
+            Point p2 = new Point(4, 5, '#');
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine("p1 = p2. p1.x = " + p1.x + ", p1.y = " + p1.y + "; p2.x = " + p2.x + ", p2.y = " + p2.y);
+
+            p1 = new Point(1, 3, '*');
+            Update(p1);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
+
+            Console.ReadLine();
         }
+
+        public static void Func1(int value)
+        {
+
+        }
+
+        public static void Func2(int value)
+        {
+            value = value + 1;
+        }
+
+        public static void Func3(int x)
+        {
+            x = x + 1;
+        }
+
+        public static void Move(Point p, int dx, int dy)
+        {
+            p.x = p.x + dx;
+            p.y = p.y + dy;
+        }
+
+        public static void Update(Point p)
+        {
+            p = new Point();
+        }
+        /*point p1 = new point(1, 3, '*');// new point()это не только объект, но и вызов ф-ии КОНСТРУКТОР
+        // класс можт работать не только с данными, но и с методами, фун-ми
+        p1.Draw();//вызываем метод из класса point для нашей точки
+
+        point p2 = new point(4, 5, '$'); 
+        p2.Draw();
+
+        Console.ReadLine();//программа не закроется сама пока пользователь не нажмет Enter*/
+    }
  
     }
-}
+
